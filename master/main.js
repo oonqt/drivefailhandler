058bb660e813f0cd66eb24a7688b26c1c1edba88
oonqt <r0blox1053@outlook.com>
2025-09-15 14:57:18 -0700

    finish slave handler

--- master/main.js
+++ master/main.js
@@ -4,17 +4,29 @@ const Logger = require('../logger');
 if (process.env.NODE_ENV === 'development') require('dotenv').config();
 
 const logger = new Logger('DriveFailHandler');
+const fs = require('fs');
 const app = express();
 
 const {
-    REQUIRED_MOUNTS,
-    PORT
+    PORT,
+    MOUNT_PATH
 } = process.env;
 
-const mounts = REQUIRED_MOUNTS.split(',').map(m => m.trim());
 
 app.get('/mounts', (req, res) => {
+    const data = fs.readFileSync('/proc/mounts', 'utf8');
+    const mounts = [];
+
+    data.split('\n').forEach(line => {
+        line = line.split(' ');
+
+        // filter only physical devices
+        if (line[0].startsWith('/dev')) mounts.push(line[1]);
+    });
+
+
+    logger.info(data);
     res.json({ mounts });
 });
 
-app.listen(PORT, () => logger.info(`Listening on ${[PORT]}`));
+app.listen(PORT, () => logger.info(`Listening on ${PORT}`));
